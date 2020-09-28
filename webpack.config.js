e30730896c18ccb67e2f3326dfe961af02990fbd
Remi Deltombe <deltombe.remi@gmail.com>
2020-09-28 10:26:40 +0200

    add dist and aggregate definitions files

--- webpack.config.js
+++ webpack.config.js
@@ -22,4 +22,21 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [new DtsBundlePlugin()]
+};
+
+
+function DtsBundlePlugin() { }
+DtsBundlePlugin.prototype.apply = function (compiler) {
+    compiler.plugin('done', function () {
+        var dts = require('dts-bundle');
+
+        dts.bundle({
+            name: 'uigl-event',
+            main: 'src/index.d.ts',
+            out: '../dist/index.d.ts',
+            removeSource: true,
+            outputAsModuleFolder: true // to use npm in-package typings
+        });
+    });
 };
